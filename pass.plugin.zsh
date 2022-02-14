bba989aa20c4bfd912babce1761e501c333abcb6
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-02-14 21:21:23 +0000

    Add pass.plugin.zsh
    Signed-off-by: Thanh Nguyen <xtnguyen605@gmail.com>

---
+++ pass.plugin.zsh
@@ -0,0 +1,38 @@
+# Ensure pass is available
+if (( ! ${+commands[pass]} )); then
+  return 1
+fi
+
+function __fzfsh_pass() {
+  local pass_dir="$HOME/.password-store"
+  fd --type=file .gpg "$pass_dir" | sed "s|^$pass_dir/||;s|.gpg$||" | fzf
+}
+
+function fzfsh::pclip() {
+  entry=${1:-$(__fzfsh_pass)}
+  [[ -z "$entry" ]] && return 1
+  pass --clip "$entry"
+}
+
+function fzfsh::pedit() {
+  entry=${1:-$(__fzfsh_pass)}
+  [[ -z "$entry" ]] && return 1
+  pass tailedit "$entry"
+}
+
+function fzfsh::potp() {
+  entry=${1:-$(__fzfsh_pass)}
+  [[ -z "$entry" ]] && return 1
+  pass otp -c "$entry"
+}
+
+function fzfsh::pshow() {
+  entry=${1:-$(__fzfsh_pass)}
+  [[ -z "$entry" ]] && return 1
+  pass show "$entry" | bat --language=yaml --paging=always
+}
+
+alias pclip='fzfsh::pclip'
+alias pedit='fzfsh::pedit'
+alias potp='fzfsh::potp'
+alias pshow='fzfsh::pshow'
