836631bc1fe684b11f9d17b19cd9675b477bbb47
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-02-14 21:13:39 +0000

    Refactor kubectl plugins
    Signed-off-by: Thanh Nguyen <xtnguyen605@gmail.com>

--- kubectl.plugin.zsh
+++ kubectl.plugin.zsh
@@ -1,61 +1,56 @@
+# Ensure kubectl is available
+if (( ! ${+commands[kubectl]} )); then
+  return 1
+fi
+
 # Shared helper to utilise FZF for Kubectl
 function __fzfsh_kubectl() {
-  query="${$(printf ' %q' "$@"):1}"       # convert input args into a single string
-  [[ ! -z "$query" ]] && query="^$query"  # prepend ^ if query isn't empty
-  fzf --header-lines=1 --query="$query" | cut -d' ' -f1
+  # convert input args into a single string
+  local query=$(printf ' %q' "$@")
+  fzf --header-lines=1 --query="${query:1}" | cut -d' ' -f1
 }
 
 # K8s - show an argo rollout
 function fzfsh::kargo() {
-  rollout=$(kubectl argo rollouts list rollout | __fzfsh_kubectl "$@")
-  [[ -z "$rollout" ]] && return 1
-  kubectl argo rollouts get rollout "$rollout"
-}
+  kubectl argo rollouts version &> /dev/null || return 1
 
-# K8s - show an argo rollouts (with context)
-function fzfsh::kargo!() {
-  context=$(kubectx | fzf)
-  [[ -z "$context" ]] && return 1
-  rollout=$(kubectl argo rollouts list rollout --context="$context" | __fzfsh_kubectl "$@")
+  if [[ "$1" == "--context" ]]; then
+    shift
+    local context=$(kubectx | fzf)
+  fi
+
+  local rollout=$(kubectl argo rollouts list rollout --context="$context" | __fzfsh_kubectl "$@")
   [[ -z "$rollout" ]] && return 1
   kubectl argo rollouts get rollout "$rollout" --context="$context"
 }
 
 # K8s - ssh to a pod
 function fzfsh::kexec() {
-  pod=$(kubectl get pods | __fzfsh_kubectl "$@")
-  [[ -z "$pod" ]] && return 1
-  kubectl exec -it "$pod" -- bash
-}
+  if [[ "$1" == "--context" ]]; then
+    shift
+    local context=$(kubectx | fzf)
+  fi
 
-# K8s - ssh to a pod (with context)
-function fzfsh::kexec!() {
-  context=$(kubectx | fzf)
-  [[ -z "$context" ]] && return 1
-  pod=$(kubectl get pods --context="$context" | __fzfsh_kubectl "$@")
+  local pod=$(kubectl get pods --context="$context" | __fzfsh_kubectl "$@")
   [[ -z "$pod" ]] && return 1
   kubectl exec -it "$pod" --context="$context" -- bash
 }
 
 # K8s - list pods
 function fzfsh::kpods() {
-  app=$(kubectl get services | __fzfsh_kubectl "$@")
-  [[ -z "$app" ]] && return 1
-  kubectl get pods -lapp="$app"
-}
+  if [[ "$1" == "--context" ]]; then
+    shift
+    local context=$(kubectx | fzf)
+  fi
 
-# K8s - list pods (with context)
-function fzfsh::kpods!() {
-  context=$(kubectx | fzf)
-  [[ -z "$context" ]] && return 1
-  app=$(kubectl get services --context="$context" | __fzfsh_kubectl "$@")
+  local app=$(kubectl get services --context="$context" | __fzfsh_kubectl "$@")
   [[ -z "$app" ]] && return 1
   kubectl get pods -lapp="$app" --context="$context"
 }
 
 alias kargo='fzfsh::kargo'
-alias kargo!='fzfsh::kargo!'
+alias kargo!='fzfsh::kargo --context'
 alias kexec='fzfsh::kexec'
-alias kexec!='fzfsh::kexec!'
+alias kexec!='fzfsh::kexec --context'
 alias kpods='fzfsh::kpods'
-alias kpods!='fzfsh::kpods!'
+alias kpods!='fzfsh::kpods --context'
