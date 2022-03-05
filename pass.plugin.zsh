1c34897498995a5a616fce584644692e00afd3fe
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-03-05 18:54:52 +0000

    Refactor fzfsh namespaces
    Signed-off-by: Thanh Nguyen <xtnguyen605@gmail.com>

--- pass.plugin.zsh
+++ pass.plugin.zsh
@@ -8,31 +8,31 @@ function __fzfsh_pass() {
   fd --type=file .gpg "$pass_dir" | sed "s|^$pass_dir/||;s|.gpg$||" | fzf
 }
 
-function fzfsh::pclip() {
+function fzfsh::pass::clip() {
   local entry=${1:-$(__fzfsh_pass)}
   [[ -z "$entry" ]] && return 1
   pass --clip "$entry"
 }
 
-function fzfsh::pedit() {
+function fzfsh::pass::edit() {
   local entry=${1:-$(__fzfsh_pass)}
   [[ -z "$entry" ]] && return 1
   pass tailedit "$entry"
 }
 
-function fzfsh::potp() {
+function fzfsh::pass::otp() {
   local entry=${1:-$(__fzfsh_pass)}
   [[ -z "$entry" ]] && return 1
   pass otp -c "$entry"
 }
 
-function fzfsh::pshow() {
+function fzfsh::pass::show() {
   local entry=${1:-$(__fzfsh_pass)}
   [[ -z "$entry" ]] && return 1
   pass show "$entry" | bat --language=yaml --paging=always
 }
 
-alias pclip='fzfsh::pclip'
-alias pedit='fzfsh::pedit'
-alias potp='fzfsh::potp'
-alias pshow='fzfsh::pshow'
+alias pclip='fzfsh::pass::clip'
+alias pedit='fzfsh::pass::edit'
+alias potp='fzfsh::pass::otp'
+alias pshow='fzfsh::pass::show'
